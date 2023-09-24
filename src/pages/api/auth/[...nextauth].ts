992b52e69d5199dbe48f9118a938fb7a7fa8dc8a
HopeFullee <john_lee0607@naver.com>
2023-09-25 03:44:05 +0900

    refactor: 리프레시 토큰 함수 리펙작업함. 대가리 출혈 심함

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,44 +4,47 @@ import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import axios from "@/lib/axios";
 
-async function refreshAccessToken(token: any) {
-  //1. access token 재발급해달라고 POST요청
-  const url = "https://github.com/login/oauth/access_token";
-  const params = {
-    grant_type: "refresh_token",
-    refresh_token: token.refreshToken,
-    client_id: "Iv1.26c8bc07ba4c65bd",
-    client_secret: "d7e2b8f39d117b7f86174c075a6dbf8d66a748d3",
-  };
+async function refreshAccessToken(provider: string, token: any) {
+  console.log("프로바이더임:" + provider);
 
-  const res = await axios.post(url, null, { params: params });
-  const refreshedTokens = await res.data;
-  if (res.status !== 200) {
-    console.log("실패", refreshedTokens);
-  }
+  try {
+    //1. access token 재발급해달라고 POST요청
+    const url = "https://github.com/login/oauth/access_token";
+    const params = {
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken,
+      client_id: "Iv1.26c8bc07ba4c65bd",
+      client_secret: "d7e2b8f39d117b7f86174c075a6dbf8d66a748d3",
+    };
 
-  //2. 재발급한거 출력해보기
-  console.log("토큰 재발급한거 : ");
-  console.log(refreshedTokens);
-  // access_token=ghu_8afeApnRAkzkBYDmshCKqq6uyKJunA1EScAS
-  // &expires_in=28800
-  // &refresh_token=ghr_IZNb9vbPyu8FnSpnP1fLP0DQPq1EVH2JLB6HMOjgBaeGbZSo3dHJihM46QM5cX1odrOUYe1OhZxc
-  // &refresh_token_expires_in=15811200
-  // &scope=
-  // &token_type=bearer
+    // 헤더 Accept를 json으로 안받으면 deafult가 URL이라 더럽게 URLSearchParams로 풀어서 return 해야함.
+    const headers = {
+      Accept: "application/json",
+    };
 
-  //3. 이걸로 새로운 토큰 만들어서 return 해주기
-  let data = new URLSearchParams(refreshedTokens);
-  if (data.get("error") == null) {
+    const res = await axios.post(url, null, {
+      headers: headers,
+      params: params,
+    });
+    const refreshedTokens = await res.data;
+
+    if (res.status !== 200) {
+      throw refreshedTokens;
+    }
+
+    //2. 이걸로 새로운 토큰 만들어서 return 해주기
     return {
       ...token,
-      accessToken: data.get("access_token"),
+      accessToken: refreshedTokens.access_token,
       accessTokenExpires:
-        Math.round(Date.now() / 1000) + Number(data.get("expires_in")),
-      refreshToken: data.get("refresh_token"),
+        Math.round(Date.now() / 1000) + refreshedTokens.expires_in,
+      refreshToken: refreshedTokens.refresh_token,
     };
-  } else {
-    return token;
+  } catch (err) {
+    return {
+      token,
+      error: "RefreshAccessTokenError",
+    };
   }
 }
 
@@ -89,30 +92,32 @@ export const authOptions: NextAuthOptions = {
 
   callbacks: {
     //4. jwt 만들 때 실행되는 코드
-    //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
     async jwt({ token, account, user }: any) {
       if (account && user) {
         return {
           acessToken: account.access_token,
           refreshToken: account.refresh_token,
           accessTokenExpires: account.expires_at,
+          provider: account.provider,
           user,
         };
       }
 
-      let timeLeft = token.accessTokenExpires - Math.round(Date.now() / 1000);
-      if (timeLeft < 60 * 60 * 8 - 10) {
-        let newJWT = await refreshAccessToken(token);
-        return newJWT;
-      } else {
-        return token;
+      const currTime = Math.round(Date.now() / 1000);
+      const shouldRefreshTime = (token.accessTokenExpires as number) - currTime;
+
+      if (shouldRefreshTime < 60 * 60 * 8 - 10) {
+        return refreshAccessToken(token.provider, token);
       }
+      // 토큰이 만료되지 않았을때는 원래사용하던 토큰을 반환
+      return token;
     },
     //5. 유저 세션이 조회될 때 마다 실행되는 코드
     async session({ session, token }: any) {
       session.user = token.user;
       session.accessToken = token.accessToken;
       session.accessTokenExpires = token.accessTokenExpires;
+      session.provider = token.provider;
       session.error = token.error;
       return session;
     },
