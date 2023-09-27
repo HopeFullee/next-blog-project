ce9d1d8cfbbed8a8de889e24b167726d5b553ac3
HopeFullee <john_lee0607@naver.com>
2023-09-27 23:50:39 +0900

    feat: google Oauth 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -58,8 +58,8 @@ export const authOptions: NextAuthOptions = {
     }),
 
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_APP_ID as string,
+      clientSecret: process.env.GOOGLE_APP_SECRET as string,
     }),
 
     CredentialsProvider({
